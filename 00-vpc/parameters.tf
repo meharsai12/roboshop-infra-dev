6c5a182b1e525f2eb4c306962a23be6026f9c459
Ravikanti Sai <RavikantiS@emids.com>
2025-07-23 14:17:29 +0530

    Terraform

--- 00-vpc/parameters.tf
+++ 00-vpc/parameters.tf
@@ -3,3 +3,24 @@ resource "aws_ssm_parameter" "vpc_id" {
   type  = "String"
   value = module.aws_vpc.vpc_id
 }
+
+resource "aws_ssm_parameter" "public_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/public_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.aws_vpc.public_subnet_ids)
+}
+
+resource "aws_ssm_parameter" "private_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/private_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.aws_vpc.private_subnet_ids)
+}
+
+
+resource "aws_ssm_parameter" "database_subnet_ids" {
+  name  = "/${var.project}/${var.environment}/public_subnet_ids"
+  type  = "StringList"
+  value = join(",", module.aws_vpc.database_subnet_ids)
+}
+
+
---
+++ 20-bastion/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  comman_tags = {
+
+    terraform = true
+    project = var.project
+    environment = var.environment
+  }
+}
