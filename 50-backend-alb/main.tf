66e55f7ac407805f6796c04d422e386b1ce85cd1
Ravikanti Sai <RavikantiS@emids.com>
2025-07-24 12:22:50 +0530

    Terraform

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -7,7 +7,8 @@ module "backend_alb" {
 
   # Security Group
   create_security_group = false # i have given security create is false because i am already created security group in 10-sg for backend alb
-  security_groups = [data.aws_ssm_parameter.backend-alb-sg.sg_id.value]
+  security_groups = [data.aws_ssm_parameter.backend-alb-sg.sg_id.value] # instaed of creating security group i have creadted separate security i called it throuh data sources by save sg id in awss sssm parameter and recalled it here
+  
   
  
   tags = merge(
